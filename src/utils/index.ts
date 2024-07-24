7c211592c16f57b8dbac370376656810b7aaec07
kiki-kanri <a470666@gmail.com>
2024-07-25 03:52:56 +0800

    feat: add utils file

---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+import { stat } from 'fs/promises';
+
+export const parseCliArgString = <T extends Record<string, string> = {}>(value: string) => {
+	const data: Record<string, string> = {};
+	for (const part of value.split(',')) {
+		const [key, value] = part.replaceAll(/\s+/g, '').split('=');
+		if (value === undefined) key !== undefined && (data.default = key);
+		else if (key) data[key] = value;
+	}
+
+	return data as T;
+};
+
+export const pathIsFile = async (path: string) => {
+	try {
+		return (await stat(path)).isFile();
+	} catch {}
+};
