bd05ab43ae90f70fe90e1b10a7e2ffa4603c20cb
kiki-kanri <a470666@gmail.com>
2024-12-13 02:05:48 +0800

    refactor: change for const to forEach inside `parseCLIArgString`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,11 +2,11 @@ import { stat } from 'node:fs/promises';
 
 export function parseCLIArgString<T extends Record<string, string> = Record<string, string>>(value: string) {
     const data: Record<string, string> = {};
-    for (const part of value.split(',')) {
+    value.split(',').forEach((part) => {
         const [key, value] = part.replaceAll(/\s+/g, '').split('=');
         if (value === undefined) key !== undefined && (data.default = key);
         else if (key) data[key] = value;
-    }
+    });
 
     return data as T;
 }
